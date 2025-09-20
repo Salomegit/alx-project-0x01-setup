9bbf81335bc6b353c0ae45c375a6abf718ebf2eb
Salomegit <sallygithinji72@gmail.com>
2025-09-20 12:54:14 +0300

    header style added

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/layout/Header";
+
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+    <Header />
+    <h1 className="text-3xl font-bold underline">
+      Users Page
+    </h1>
+    </div>
+  )
+}
+
+export default User
